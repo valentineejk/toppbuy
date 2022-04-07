66a8add54b1455944a3816b069296a1aad5eff88
valentineejk <valentineejk@gmail.com>
2022-04-07 21:41:01 +0100

    working on header

---
+++ components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Navbar=() => {
+    return (
+
+        <div className=' py-5 '>
+
+            <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-200 '>
+                <div className='container flex flex-wrap justify-between items-center mx-auto'>
+                    <a href="https://flowbite.com" className="flex items-center">
+
+                        <Image src="/logo.svg" alt="Vercel Logo" width={150} height={80} />
+
+                        {/* <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
+                    </a>
+
+
+                    <div className='flex md:order-2'>
+                        <button type="button" className="flex md:order-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+
+                    </div>
+
+                    {/* <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        </li>
+                    </ul>
+                </div> */}
+                </div>
+
+
+            </nav>
+        </div>
+
+    )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
 import '../styles/globals.css'
+import {StoreProvider} from '../components/Store'
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+Router.events.on('routeChangeStart',() => nProgress.start());
+Router.events.on('routeChangeComplete',() => nProgress.done());
+Router.events.on('routeChangeError',() => nProgress.done());
+
+
+
+function MyApp({Component,pageProps}) {
+  return (<StoreProvider>
+
+    <Component {...pageProps} />
+
+  </StoreProvider>);
+
 }
 
 export default MyApp
+
+
+MyApp.getInitialProps=async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  };
+};
